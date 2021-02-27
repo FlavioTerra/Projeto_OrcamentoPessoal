eec45815c212324c58cefefa03862d7af8dd880e
Flavio Moreira Terra <FlavioTerraMoreira@gmail.com>
2021-02-27 03:37:24 -0300

    Adicionado a classe BD e cadastrarDespesa

--- script.js
+++ script.js
@@ -8,6 +8,37 @@ class Despesa {
         this.descricao = descricao;
         this.valor = valor;
     }
+
+    validarDados() {
+        for(let x in this) {
+            if(this[x] === undefined || this[x] === null || this[x] === '') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class BD {
+
+    constructor() {
+        let id = localStorage.getItem('id');
+
+        if(id === null) {
+            localStorage.setItem('id', 0);
+        }
+    }
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id');
+        return parseInt(proximoId) + 1;
+    }
+
+    gravar(desp) {
+        let id = this.getProximoId();
+        localStorage.setItem(id, JSON.stringify(desp));
+        localStorage.setItem('id', id);
+    }
 }
 
 function cadastrarDespesa() {
@@ -20,5 +51,22 @@ function cadastrarDespesa() {
     let valor = document.getElementById('valor');
 
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value);
-    console.log(despesa);
+    let bd = new BD();
+
+    if(despesa.validarDados()) {
+        //bd.gravar(despesa);
+        document.getElementById('modalTitulo').innerHTML = 'Sucesso na Gravação';
+        document.getElementById('modalTitulo').className = 'modal-title text-success';
+        document.getElementById('modalTexto').innerHTML = 'A despesa foi cadastrada com sucesso!';
+        document.getElementById('botaoVoltar').className = 'btn btn-success';
+        //Diálogo de sucesso
+        $('#modalRegistro').modal('show');
+    } else {
+        document.getElementById('modalTitulo').innerHTML = 'Erro na Gravação';
+        document.getElementById('modalTitulo').className = 'modal-title text-danger';
+        document.getElementById('modalTexto').innerHTML = 'Existem campos obrigatórios que não foram preenchidos!';
+        document.getElementById('modalBotao').className = 'btn btn-danger';
+        //Diálogo de erro
+        $('#modalRegistro').modal('show');
+    }
 }
